c4644135a46c6939a6f2fa358a33ea124dad965d
Gigih Satriono <gigih.satriono@mncgroup.com>
2026-04-23 14:44:18 +0700

    new update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // ⚠️ UPDATE THIS after backend deployed to Railway
   // Format: https://<backend-domain>.up.railway.app/api
-  apiBaseUrl: 'https://REPLACE-WITH-BACKEND-URL.up.railway.app/api'
+  apiBaseUrl: 'https://task-backend-production-eb49.up.railway.app/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000/api'
+  apiBaseUrl: 'https://task-backend-production-eb49.up.railway.app/api'
 };
